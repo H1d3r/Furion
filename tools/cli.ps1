e3f9f8cc4d8cd9268c6df9a3407bda6b70b68b09
MonkSoul <monksoul@outlook.com>
2025-03-02 00:13:43 +0800

    😊 发布 `v4.9.7.20` 版本，新增流变对象 `AddEvent` 动态订阅事件方法

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.19";
+$FurTools = "Furion Tools v4.9.7.20";
 
 # 输出信息
 $copyright = @"
