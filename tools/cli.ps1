dbd1dd33a12a963dc2caf807c4cb12a8e9355868
MonkSoul <monksoul@outlook.com>
2025-03-03 02:18:49 +0800

    😊 发布 `v4.9.7.21` 版本，新增 `HTTP` 远程请求设置路径片段功能 * 修复 `HTTP` 远程请求移除查询参数无效问题

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.20";
+$FurTools = "Furion Tools v4.9.7.21";
 
 # 输出信息
 $copyright = @"
