df879321a96f50f5ebf84c6e134d5a65dac9ea68
MonkSoul <monksoul@outlook.com>
2025-03-13 17:08:40 +0800

    😊 发布 `v4.9.7.24` 版本，改进 `HTTP` 远程请求、新增日志时间格式化配置和 `HostApplicationBuilder.Inject()` 初始化

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.23";
+$FurTools = "Furion Tools v4.9.7.24";
 
 # 输出信息
 $copyright = @"
