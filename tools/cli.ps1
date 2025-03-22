352996fbb3af9bfdacaf51c8693355bcb7af8a8c
MonkSoul <monksoul@outlook.com>
2025-03-22 17:31:53 +0800

    😊 发布 `v4.9.7.28` 版本，修复 `HTTP` 远程请求启用强制 `IPv4` 之后出现异常问题

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.27";
+$FurTools = "Furion Tools v4.9.7.28";
 
 # 输出信息
 $copyright = @"
