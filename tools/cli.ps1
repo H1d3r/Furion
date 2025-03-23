c4aa1fe488a929c4995dc60ac1d54ae4d75dfd99
MonkSoul <monksoul@outlook.com>
2025-03-24 02:12:40 +0800

    😊 发布 `v4.9.7.32` 版本，修复流变对象转换问题

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.31";
+$FurTools = "Furion Tools v4.9.7.32";
 
 # 输出信息
 $copyright = @"
