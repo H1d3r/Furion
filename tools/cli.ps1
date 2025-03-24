90fe8e73741194715dbd678dbcecfa6bcdd564ba
MonkSoul <monksoul@outlook.com>
2025-03-24 18:24:55 +0800

    😊 发布 `v4.9.7.33` 版本，新增 `HTTP` 远程请求添加授权标头重载方法

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.32";
+$FurTools = "Furion Tools v4.9.7.33";
 
 # 输出信息
 $copyright = @"
