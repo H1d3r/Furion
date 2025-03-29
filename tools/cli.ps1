78bfd1cc181b40a77d88e17610c25e0d7c207ff9
MonkSoul <monksoul@outlook.com>
2025-03-29 13:36:40 +0800

    😊 发布 `v4.9.7.35` 版本，新增 流变对象支持 `MVC` 应用 `URL` 表单数据转换

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.34";
+$FurTools = "Furion Tools v4.9.7.35";
 
 # 输出信息
 $copyright = @"
