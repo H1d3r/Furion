aa3601c778213503899bb724c0feb85dd3b71680
MonkSoul <monksoul@outlook.com>
2025-04-20 04:10:46 +0800

    😊 发布 `v4.9.7.47` 版本，适配 `.NET 10 SDK` 、改进 `HTTP` 远程请求和流变对象

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.46";
+$FurTools = "Furion Tools v4.9.7.47";
 
 # 输出信息
 $copyright = @"
