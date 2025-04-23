d7bdb1623bdc99705eec022bb87ecea81eef0e0b
MonkSoul <monksoul@outlook.com>
2025-04-24 00:02:06 +0800

    😊 发布 `v4.9.7.49` 版本，修复流变对象转换为 `object` 或与之相关的泛型时出现 `JsonElement` 类型

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.48";
+$FurTools = "Furion Tools v4.9.7.49";
 
 # 输出信息
 $copyright = @"
