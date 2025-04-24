8bffea318488d511a24c250cbc2ed40dac99a927
MonkSoul <monksoul@outlook.com>
2025-04-25 00:34:44 +0800

    😊 发布 `v4.9.7.50` 版本，修复上一版本导致 `HTTP` 远程请求反序列化出现内存溢出 `OOM` 问题 😒😒😒

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.49";
+$FurTools = "Furion Tools v4.9.7.50";
 
 # 输出信息
 $copyright = @"
