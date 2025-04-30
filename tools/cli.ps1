eb5c1f52b7e07c11cd26db592c585241b4734d10
MonkSoul <monksoul@outlook.com>
2025-04-30 22:41:32 +0800

    😊 发布 `v4.9.7.57` 版本，修复 `Oracle` 数据库迁移到达梦数据库的所有发现问题

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.55";
+$FurTools = "Furion Tools v4.9.7.57";
 
 # 输出信息
 $copyright = @"
