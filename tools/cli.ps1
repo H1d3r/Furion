91788c73ed9818dd66a683a564689f2e470ca71b
MonkSoul <monksoul@outlook.com>
2025-05-25 21:21:52 +0800

    😊 发布 `v4.9.7.75` 版本，移除 `MiniProfiler` 依赖及相关代码

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.74";
+$FurTools = "Furion Tools v4.9.7.75";
 
 # 输出信息
 $copyright = @"
