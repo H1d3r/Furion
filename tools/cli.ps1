8c9166a62b21b1c6ff646831c7f0ef8e739e5d7e
MonkSoul <monksoul@outlook.com>
2025-06-01 05:59:42 +0800

    😊 发布 `v4.9.7.78` 版本，新增定时任务看板搜索功能

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.77";
+$FurTools = "Furion Tools v4.9.7.78";
 
 # 输出信息
 $copyright = @"
