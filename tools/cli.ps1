b67a772751f73f0f085d7cafabaea34afb09e6d8
MonkSoul <monksoul@outlook.com>
2025-06-02 23:16:33 +0800

    😊 发布 `v4.9.7.79` 版本，修复定时任务看板出现客户端错误导致空白问题 #ICBYKW

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.78";
+$FurTools = "Furion Tools v4.9.7.79";
 
 # 输出信息
 $copyright = @"
