762555b035ce1408fb7bcc2652f10e4fcfd09a41
MonkSoul <monksoul@outlook.com>
2025-06-04 14:32:52 +0800

    😊 发布 `v4.9.7.81` 版本，移除 `Furion.Extras.Logging.Serilog` 拓展包

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.80";
+$FurTools = "Furion Tools v4.9.7.81";
 
 # 输出信息
 $copyright = @"
