6e8f8243036e08a8d28ae36e9a6d58bab9645868
MonkSoul <monksoul@outlook.com>
2025-06-13 00:22:08 +0800

    😊 发布 `v4.9.7.86` 版本，修复流变对象转换 `object` 对象时丢失配置选项问题

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.85";
+$FurTools = "Furion Tools v4.9.7.86";
 
 # 输出信息
 $copyright = @"
