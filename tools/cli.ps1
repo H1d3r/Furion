f547a58876985291c8bdcf48b8a3f42a75b957af
MonkSoul <monksoul@outlook.com>
2025-06-16 20:23:52 +0800

    😊 发布 `v4.9.7.88` 版本，新增定时任务看板支持配置默认填充用户名和密码

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.87";
+$FurTools = "Furion Tools v4.9.7.88";
 
 # 输出信息
 $copyright = @"
