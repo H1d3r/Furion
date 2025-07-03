4bf30745157572652ef7c9c5362ce53f152491e6
MonkSoul <monksoul@outlook.com>
2025-07-03 23:25:38 +0800

    😊 发布 `v4.9.7.92` 版本，修复定时任务取消 `CPU` 休眠操作并发问题导致服务终止

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.91";
+$FurTools = "Furion Tools v4.9.7.92";
 
 # 输出信息
 $copyright = @"
