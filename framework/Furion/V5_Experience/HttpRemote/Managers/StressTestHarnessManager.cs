461567045a1dfced8f4a12b2af028f15b653af21
MonkSoul <monksoul@outlook.com>
2025-07-09 20:09:51 +0800

    😊 调整 `HTTP` 远程请求拓展功能接口设计，移除 `Action<HttpRequestBuilder>`，新增 `WithRequest(Action<HttpRequestBuilder>)`

--- framework/Furion/V5_Experience/HttpRemote/Managers/StressTestHarnessManager.cs
+++ framework/Furion/V5_Experience/HttpRemote/Managers/StressTestHarnessManager.cs
@@ -49,10 +49,8 @@ internal sealed class StressTestHarnessManager
     /// <param name="httpStressTestHarnessBuilder">
     ///     <see cref="HttpStressTestHarnessBuilder" />
     /// </param>
-    /// <param name="configure">自定义配置委托</param>
     internal StressTestHarnessManager(IHttpRemoteService httpRemoteService,
-        HttpStressTestHarnessBuilder httpStressTestHarnessBuilder,
-        Action<HttpRequestBuilder>? configure = null)
+        HttpStressTestHarnessBuilder httpStressTestHarnessBuilder)
     {
         // 空检查
         ArgumentNullException.ThrowIfNull(httpRemoteService);
@@ -63,7 +61,7 @@ internal sealed class StressTestHarnessManager
 
         // 构建 HttpRequestBuilder 实例
         RequestBuilder = httpStressTestHarnessBuilder.Build(httpRemoteService.ServiceProvider
-            .GetRequiredService<IOptions<HttpRemoteOptions>>().Value, configure);
+            .GetRequiredService<IOptions<HttpRemoteOptions>>().Value);
     }
 
     /// <summary>
