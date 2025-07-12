446a0d8a13b2f63f382cd693e51ef94e2e055df3
MonkSoul <monksoul@outlook.com>
2025-07-12 16:57:16 +0800

    😊 发布 `v4.9.7.97` 版本，改进流变对象删除数据操作

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.96";
+$FurTools = "Furion Tools v4.9.7.97";
 
 # 输出信息
 $copyright = @"
