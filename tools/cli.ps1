56689884dc14de6c4dcfe9eb9d16ed538a38db27
MonkSoul <monksoul@outlook.com>
2025-07-23 12:00:04 +0800

    😊 发布 `v4.9.7.102` 版本，修复定时任务错误使用 `GC` 导致休眠后无法被唤醒问题

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.101";
+$FurTools = "Furion Tools v4.9.7.102";
 
 # 输出信息
 $copyright = @"
