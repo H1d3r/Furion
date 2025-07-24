81a453fd4dc4deb57148b262076257b102002b67
MonkSoul <monksoul@outlook.com>
2025-07-24 15:18:32 +0800

    😊 发布 `v4.9.7.104` 版本，修复 `HTTP` 远程请求转发 `HttpContext` 出现禁用缓存无效问题

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.103";
+$FurTools = "Furion Tools v4.9.7.104";
 
 # 输出信息
 $copyright = @"
