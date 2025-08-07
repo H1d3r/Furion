cfc35b673bdf5ea6be5fc1b6f6d3d0139c908cff
MonkSoul <monksoul@outlook.com>
2025-08-07 20:15:44 +0800

    😊 发布 `v4.9.7.108` 版本，改进定时任务 `MarcoAt` 作业触发器特性

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.107";
+$FurTools = "Furion Tools v4.9.7.108";
 
 # 输出信息
 $copyright = @"
