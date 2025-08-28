0bf8b3e4825fc66ec2f39b7895b34e39e047c9b6
MonkSoul <monksoul@outlook.com>
2025-08-28 21:23:09 +0800

    😊 发布 `v4.9.7.113` 版本，改进 `Swagger` 登录和 `HTTP` 远程请求文件下载

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.112";
+$FurTools = "Furion Tools v4.9.7.113";
 
 # 输出信息
 $copyright = @"
