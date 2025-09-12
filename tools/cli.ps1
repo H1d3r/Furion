89bf8f539914f9afb5cce5c61fb94848ffa881a1
MonkSoul <monksoul@outlook.com>
2025-09-12 23:21:12 +0800

    😊 发布 `v4.9.7.121` 版本，修复打开定时任务看板出现警告日志问题 #ICXMRV

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.120";
+$FurTools = "Furion Tools v4.9.7.121";
 
 # 输出信息
 $copyright = @"
