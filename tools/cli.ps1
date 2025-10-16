6779c986431b08c9ea36612d062aa7b749536007
MonkSoul <monksoul@outlook.com>
2025-10-16 23:32:08 +0800

    😊 发布 `v4.9.7.131` 版本，新增 `HTTP` 压力测试支持添加动态 `URL` 参数和禁用 `HTTP` 缓存便捷操作

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.130";
+$FurTools = "Furion Tools v4.9.7.131";
 
 # 输出信息
 $copyright = @"
