e7272d1f6033cfb675d50fb4534911e4b184415a
MonkSoul <monksoul@outlook.com>
2025-10-22 15:08:48 +0800

    😊 发布 `v4.9.7.133` 版本，修复手动模型校验不支持带解析服务的自定义验证特性的问题

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.132";
+$FurTools = "Furion Tools v4.9.7.133";
 
 # 输出信息
 $copyright = @"
