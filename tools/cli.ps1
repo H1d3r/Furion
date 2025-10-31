72e5e5a6defca38822ac416a2a3d5ca8fef5c645
MonkSoul <monksoul@outlook.com>
2025-10-31 16:24:23 +0800

    😊 发布 `v4.9.7.136` 版本，修复因 `v4.9.7.127` 版本导致 `TP.Wrapper` 出现递归死循环问题 #ID4DWV #ICZEAX

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.135";
+$FurTools = "Furion Tools v4.9.7.136";
 
 # 输出信息
 $copyright = @"
