51168621dccb89e16eddba0ec44aea62f693cc80
MonkSoul <monksoul@outlook.com>
2025-11-07 00:01:46 +0800

    😊 新增 `HTTP` 远程请求支持表单名称命名策略或自定义转换器

--- framework/Furion/V5_Experience/HttpRemote/Constants/FormNamingPolicy.cs
+++ framework/Furion/V5_Experience/HttpRemote/Constants/FormNamingPolicy.cs
@@ -26,37 +26,37 @@
 namespace Furion.HttpRemote;
 
 /// <summary>
-///     HTTP 声明式多部分表单内容特性
+///     表单名称命名策略（转换器）
 /// </summary>
-/// <remarks>需配合 <see cref="MultipartAttribute" /> 使用。</remarks>
-[AttributeUsage(AttributeTargets.Method)]
-public sealed class MultipartFormAttribute : Attribute
+public enum FormNamingPolicy
 {
     /// <summary>
-    ///     <inheritdoc cref="MultipartFormAttribute" />
+    ///     缺省值
     /// </summary>
-    public MultipartFormAttribute()
-    {
-    }
+    None = 0,
 
     /// <summary>
-    ///     <inheritdoc cref="MultipartFormAttribute" />
+    ///     小驼峰命名法
     /// </summary>
-    /// <param name="boundary">多部分表单内容的边界</param>
-    public MultipartFormAttribute(string? boundary) => Boundary = boundary;
+    CamelCase,
 
     /// <summary>
-    ///     多部分表单内容的边界
+    ///     小写蛇形命名法
     /// </summary>
-    public string? Boundary { get; set; } = $"--------------------------{DateTime.Now.Ticks:x}";
+    SnakeCaseLower,
 
     /// <summary>
-    ///     是否移除默认的多部分内容的 <c>Content-Type</c>
+    ///     大写蛇形命名法
     /// </summary>
-    /// <remarks>默认值为：<c>true</c>。</remarks>
-    public bool OmitContentType { get; set; } = true;
+    SnakeCaseUpper,
 
-    /// <inheritdoc cref="FormNamingPolicy" />
-    /// <remarks>默认值为：<see cref="FormNamingPolicy.None" />。</remarks>
-    public FormNamingPolicy NamingPolicy { get; set; } = FormNamingPolicy.None;
+    /// <summary>
+    ///     小写短横线命名法
+    /// </summary>
+    KebabCaseLower,
+
+    /// <summary>
+    ///     写短横线命名法
+    /// </summary>
+    KebabCaseUpper
 }
