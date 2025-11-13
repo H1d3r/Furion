907b2f93a1b35f597080a0263f805746bb68b0cb
MonkSoul <monksoul@outlook.com>
2025-11-13 22:47:04 +0800

    😊 发布 `v4.9.7.205` 版本，修复 `.NET10` 版本的 `Swagger JSON` 在 `Swagger Editor` 的导入错误

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.204";
+$FurTools = "Furion Tools v4.9.7.205";
 
 # 输出信息
 $copyright = @"
