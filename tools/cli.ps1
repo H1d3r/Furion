7ad4fa489d2167700cf146b2425268450560a41a
MonkSoul <monksoul@outlook.com>
2025-11-17 18:21:18 +0800

    😊 发布 `v4.9.7.209` 版本，修复脚手架不支持 `Visual Studio 2026` 问题 #ID70D4

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.208";
+$FurTools = "Furion Tools v4.9.7.209";
 
 # 输出信息
 $copyright = @"
