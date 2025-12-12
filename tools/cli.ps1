2cb13d1c91b90cf60e8fc78f80be7bec4cd1b2d8
MonkSoul <monksoul@outlook.com>
2025-12-12 15:46:21 +0800

    😊 发布 `v4.9.7.226` 版本，改进 新版本数据验证集成方式

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.225";
+$FurTools = "Furion Tools v4.9.7.226";
 
 # 输出信息
 $copyright = @"
