d92ae7c03e7db876f41da974a74a1fbf852f6811
MonkSoul <monksoul@outlook.com>
2025-12-14 04:30:34 +0800

    😊 预留 新版本数据验证异步接口

---
+++ framework/Furion.Pure/V5_Experience/Validation/Interfaces/IAsyncValidator.cs
@@ -0,0 +1,64 @@
+﻿// ------------------------------------------------------------------------
+// 版权信息
+// 版权归百小僧及百签科技（广东）有限公司所有。
+// 所有权利保留。
+// 官方网站：https://baiqian.com
+//
+// 许可证信息
+// Furion 项目主要遵循 MIT 许可证和 Apache 许可证（版本 2.0）进行分发和使用。
+// 许可证的完整文本可以在源代码树根目录中的 LICENSE-APACHE 和 LICENSE-MIT 文件中找到。
+// 官方网站：https://furion.net
+//
+// 使用条款
+// 使用本代码应遵守相关法律法规和许可证的要求。
+//
+// 免责声明
+// 对于因使用本代码而产生的任何直接、间接、偶然、特殊或后果性损害，我们不承担任何责任。
+//
+// 其他重要信息
+// Furion 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。
+// 有关 Furion 项目的其他详细信息，请参阅位于源代码树根目录中的 COPYRIGHT 和 DISCLAIMER 文件。
+//
+// 更多信息
+// 请访问 https://gitee.com/dotnetchina/Furion 获取更多关于 Furion 项目的许可证和版权信息。
+// ------------------------------------------------------------------------
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Furion.Validation;
+
+/// <summary>
+///     异步验证器接口
+/// </summary>
+public interface IAsyncValidator
+{
+    /// <summary>
+    ///     检查对象合法性
+    /// </summary>
+    /// <param name="value">对象</param>
+    /// <returns>
+    ///     <see cref="bool" />
+    /// </returns>
+    Task<bool> IsValidAsync(object? value);
+
+    /// <summary>
+    ///     获取对象验证结果集合
+    /// </summary>
+    /// <param name="value">对象</param>
+    /// <param name="name">显示名称</param>
+    /// <param name="memberNames">成员名称列表</param>
+    /// <returns>
+    ///     <see cref="List{T}" />
+    /// </returns>
+    Task<List<ValidationResult>?> GetValidationResultsAsync(object? value, string name,
+        IEnumerable<string>? memberNames = null);
+
+    /// <summary>
+    ///     验证指定的对象
+    /// </summary>
+    /// <param name="value">对象</param>
+    /// <param name="name">显示名称</param>
+    /// <param name="memberNames">成员名称列表</param>
+    /// <exception cref="ValidationException"></exception>
+    Task Validate(object? value, string name, IEnumerable<string>? memberNames = null);
+}
