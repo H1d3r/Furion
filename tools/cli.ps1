36eb37ad2eac9c057ec7f64200d16f85a3d4712a
MonkSoul <monksoul@outlook.com>
2025-12-16 23:20:39 +0800

    😊 发布 `v4.9.7.229` 版本，修复文件日志不能滚动删除问题

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.228";
+$FurTools = "Furion Tools v4.9.7.229";
 
 # 输出信息
 $copyright = @"
