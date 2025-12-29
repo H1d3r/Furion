e58ec737a724447b3a0eebe63c0c06c52bb0c69b
MonkSoul <monksoul@outlook.com>
2025-12-29 23:01:41 +0800

    😊 发布 `v4.9.7.237` 版本，新增新版本数据验证固定长度验证器

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.236";
+$FurTools = "Furion Tools v4.9.7.237";
 
 # 输出信息
 $copyright = @"
