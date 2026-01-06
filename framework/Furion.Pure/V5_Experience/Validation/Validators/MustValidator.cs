25d580e582081a053fcce0c676daa0416c4ec79d
MonkSoul <monksoul@outlook.com>
2026-01-07 03:28:44 +0800

    😊 改进 新版本数据验证集合验证器

--- framework/Furion.Pure/V5_Experience/Validation/Validators/MustValidator.cs
+++ framework/Furion.Pure/V5_Experience/Validation/Validators/MustValidator.cs
@@ -32,28 +32,15 @@ namespace Furion.Validation;
 /// <summary>
 ///     <see cref="MustValidator{T}" /> 内部静态类
 /// </summary>
-/// <remarks>可通过 <see cref="Must.False" /> 设置不满足条件时的异常消息。</remarks>
+/// <remarks>可通过 <see cref="Must.WithMessage" /> 设置不满足条件时的异常消息。</remarks>
 public static class Must
 {
     /// <summary>
-    ///     抛出 <see cref="ValidatorException" /> 异常
+    ///     设置错误信息
     /// </summary>
     /// <param name="message">错误消息</param>
     [DoesNotReturn]
-    public static void False(string message) => ValidatorException.Throw(message);
-
-    /// <summary>
-    ///     抛出 <see cref="ValidatorException" /> 异常
-    /// </summary>
-    /// <param name="condition">条件</param>
-    /// <param name="message">错误消息</param>
-    public static void FalseIf(bool condition, string message)
-    {
-        if (condition)
-        {
-            ValidatorException.Throw(message);
-        }
-    }
+    public static void WithMessage(string message) => ValidatorException.Throw(message);
 }
 
 /// <summary>
@@ -98,7 +85,7 @@ public class MustValidator<T> : ValidatorBase<T>
         {
             return Condition(instance!, validationContext);
         }
-        // 检查验证器内部是否抛出 ValidatorException 异常
+        // 检查是否是 ValidatorException 异常
         catch (ValidatorException)
         {
             return false;
@@ -121,14 +108,14 @@ public class MustValidator<T> : ValidatorBase<T>
                 new ValidationResult(FormatErrorMessage(validationContext.DisplayName), validationContext.MemberNames)
             ];
         }
-        // 检查验证器内部是否抛出 ValidatorException 异常
+        // 检查是否是 ValidatorException 异常
         catch (ValidatorException e)
         {
             return
             [
                 new ValidationResult(
-                    string.Format(CultureInfo.CurrentCulture, e.Message, validationContext?.DisplayName),
-                    validationContext?.MemberNames)
+                    string.Format(CultureInfo.CurrentCulture, e.Message, validationContext.DisplayName),
+                    validationContext.MemberNames)
             ];
         }
     }
@@ -142,17 +129,17 @@ public class MustValidator<T> : ValidatorBase<T>
             if (!Condition(instance!, validationContext))
             {
                 throw new ValidationException(
-                    new ValidationResult(FormatErrorMessage(validationContext?.DisplayName!),
-                        validationContext?.MemberNames), null, instance);
+                    new ValidationResult(FormatErrorMessage(validationContext.DisplayName),
+                        validationContext.MemberNames), null, instance);
             }
         }
-        // 检查验证器内部是否抛出 ValidatorException 异常
+        // 检查是否是 ValidatorException 异常
         catch (ValidatorException e)
         {
             throw new ValidationException(
                 new ValidationResult(
-                    string.Format(CultureInfo.CurrentCulture, e.Message, validationContext?.DisplayName),
-                    validationContext?.MemberNames), null, instance);
+                    string.Format(CultureInfo.CurrentCulture, e.Message, validationContext.DisplayName),
+                    validationContext.MemberNames), null, instance);
         }
     }
 }
--- framework/Furion/V5_Experience/Validation/Fluent/Interfaces/IPropertyValidator.cs
+++ framework/Furion/V5_Experience/Validation/Fluent/Interfaces/IPropertyValidator.cs
@@ -31,3 +31,21 @@ namespace Furion.Validation;
 /// <typeparam name="T">对象类型</typeparam>
 public interface IPropertyValidator<T> : IObjectValidator<T>, IMemberPathRepairable, IValidationAnnotationsConfigurable,
     IPropertyValidatorCloneable<T>;
+
+/// <summary>
+///     定义属性验证器可克隆行为
+/// </summary>
+/// <typeparam name="T">对象类型</typeparam>
+public interface IPropertyValidatorCloneable<T>
+{
+    /// <summary>
+    ///     克隆
+    /// </summary>
+    /// <param name="objectValidator">
+    ///     <see cref="ObjectValidator{T}" />
+    /// </param>
+    /// <returns>
+    ///     <see cref="IPropertyValidator{T}" />
+    /// </returns>
+    IPropertyValidator<T> Clone(ObjectValidator<T> objectValidator);
+}
