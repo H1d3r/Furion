2dc1e98fef418be9f51c48ce4e446bea4e577abb
MonkSoul <monksoul@outlook.com>
2026-01-16 05:14:35 +0800

    😊 发布 `v4.9.7.248` 版本，改进任务队列模块（为全新版本提前准备）

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.7.247";
+$FurTools = "Furion Tools v4.9.7.248";
 
 # 输出信息
 $copyright = @"
