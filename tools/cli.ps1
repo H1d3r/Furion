7ded8526369d0831ef2235e109b1347b77a52f0a
MonkSoul <monksoul@outlook.com>
2026-01-29 14:32:31 +0800

    😊 发布 `v4.9.8.10` 版本，改近脱敏模块和数据验证模块

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.8.9";
+$FurTools = "Furion Tools v4.9.8.10";
 
 # 输出信息
 $copyright = @"
