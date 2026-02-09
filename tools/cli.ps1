ae9cbf03fa270965767da9563b9acbf08f902fea
MonkSoul <monksoul@outlook.com>
2026-02-09 17:38:41 +0800

    😊 发布 `v4.9.8.15` 版本，新增 `HTTP` 远程请求发送未编码的表单数据

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.8.14";
+$FurTools = "Furion Tools v4.9.8.15";
 
 # 输出信息
 $copyright = @"
