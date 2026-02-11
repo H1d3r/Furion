d50597be313c3a9aa8548125216ee770dc9ad814
MonkSoul <monksoul@outlook.com>
2026-02-11 11:03:31 +0800

    😊 改进 `HTTP` 远程请求调试日志输出

--- framework/Furion/V5_Experience/HttpRemote/Extensions/HttpContextExtensions.cs
+++ framework/Furion/V5_Experience/HttpRemote/Extensions/HttpContextExtensions.cs
@@ -628,7 +628,7 @@ public static partial class HttpContextExtensions
         if (httpResponseMessage is null)
         {
             // 输出调试信息
-            Debugging.Error("The response content was not read, as it was empty.");
+            Debugging.Error(Constants.HTTP_RESPONSE_MESSAGE_ISNULL_MESSAGE);
 
             return;
         }
