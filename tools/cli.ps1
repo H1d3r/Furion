352928175eea68927d0b5293119915400a1649f2
MonkSoul <monksoul@outlook.com>
2026-03-15 04:08:26 +0800

    😊 发布 `v4.9.8.24` 版本 ，新增 `JWT` 鉴权支持 `RS*/PS*/ES*` 加密算法

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.8.23";
+$FurTools = "Furion Tools v4.9.8.24";
 
 # 输出信息
 $copyright = @"
