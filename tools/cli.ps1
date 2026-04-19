fc7fef77c42968676625db01a56c5bea69ce9808
MonkSoul <monksoul@outlook.com>
2026-04-19 20:42:02 +0800

    😊 发布 `v4.9.8.47` 版本，优化视图引擎编译性能和修复生成缓存键存在不确定性

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.8.46";
+$FurTools = "Furion Tools v4.9.8.47";
 
 # 输出信息
 $copyright = @"
