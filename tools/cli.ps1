5c1cab0572f283ad044d18bc5155f0c9413d027e
MonkSoul <monksoul@outlook.com>
2026-04-25 22:28:18 +0800

    😊 发布 `v4.9.8.51` 版本，改进任务队列、事件总线和定时任务服务

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.8.50";
+$FurTools = "Furion Tools v4.9.8.51";
 
 # 输出信息
 $copyright = @"
