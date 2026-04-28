fa2d65b9ee299cb5641d3ffa487c3cbf46fa77cb
MonkSoul <monksoul@outlook.com>
2026-04-28 22:26:37 +0800

    😊 完善 数据验证模块规则集说明，新增实用拓展方法

--- framework/Furion.Pure/V5_Experience/Validation/Fluent/Validators/ObjectValidator.cs
+++ framework/Furion.Pure/V5_Experience/Validation/Fluent/Validators/ObjectValidator.cs
@@ -286,7 +286,6 @@ public class ObjectValidator<T> : ValidatorBase<T>, IObjectValidator<T>, IMember
         }
 
         // 检查是否设置了对象级别验证器
-        // ReSharper disable once UseNullPropagation
         _objectValidator?.Validate(instance, resolvedRuleSets);
     }
 
@@ -498,7 +497,7 @@ public class ObjectValidator<T> : ValidatorBase<T>, IObjectValidator<T>, IMember
     /// <summary>
     ///     在指定规则集上下文中为指定属性配置验证规则
     /// </summary>
-    /// <param name="ruleSets">规则集</param>
+    /// <param name="ruleSets">规则集。未设置规则集时，默认值为 <c>null</c>（匹配默认规则集请使用 <c>null</c>，空字符串无效）</param>
     /// <param name="setAction">自定义配置委托</param>
     /// <returns>
     ///     <see cref="ObjectValidator{T}" />
@@ -525,7 +524,7 @@ public class ObjectValidator<T> : ValidatorBase<T>, IObjectValidator<T>, IMember
     /// <summary>
     ///     在指定规则集上下文中为指定属性配置验证规则
     /// </summary>
-    /// <param name="ruleSets">规则集</param>
+    /// <param name="ruleSets">规则集。未设置规则集时，默认值为 <c>null</c>（匹配默认规则集请使用 <c>null</c>，空字符串无效）</param>
     /// <param name="setAction">自定义配置委托</param>
     /// <returns>
     ///     <see cref="ObjectValidator{T}" />
@@ -895,7 +894,7 @@ public class ObjectValidator<T> : ValidatorBase<T>, IObjectValidator<T>, IMember
     /// <summary>
     ///     解析验证时使用的规则集
     /// </summary>
-    /// <param name="ruleSets">规则集</param>
+    /// <param name="ruleSets">规则集。未设置规则集时，默认值为 <c>null</c>（匹配默认规则集请使用 <c>null</c>，空字符串无效）</param>
     /// <returns><see cref="string" />列表</returns>
     internal string?[]? ResolveValidationRuleSets(string?[]? ruleSets) =>
         // 优先使用显式传入的规则集，否则从验证数据上下文中解析
@@ -947,7 +946,7 @@ public class ObjectValidator<T> : ValidatorBase<T>, IObjectValidator<T>, IMember
     ///     创建 <see cref="ValidationContext{T}" /> 实例
     /// </summary>
     /// <param name="instance">对象</param>
-    /// <param name="ruleSets">规则集</param>
+    /// <param name="ruleSets">规则集。未设置规则集时，默认值为 <c>null</c>（匹配默认规则集请使用 <c>null</c>，空字符串无效）</param>
     /// <returns>
     ///     <see cref="ValidationContext{T}" />
     /// </returns>
