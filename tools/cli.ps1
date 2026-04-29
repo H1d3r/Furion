6c25d4160cde90a583c8205a40e3b2472f3e6275
MonkSoul <monksoul@outlook.com>
2026-04-29 21:55:08 +0800

    😊 发布 `v4.9.8.56` 版本，修复 `JWT` 授权导致 `SignalR/WebSocket` 返回 `101` 状态码问题

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.8.55";
+$FurTools = "Furion Tools v4.9.8.56";
 
 # 输出信息
 $copyright = @"
