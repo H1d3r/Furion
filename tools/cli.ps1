00ec02ff8adab76c6ae54d03af1ed5e535485fc9
MonkSoul <monksoul@outlook.com>
2026-05-16 23:22:23 +0800

    😊 发布 `v4.9.8.71` 版本，事件总线性能提升 `100倍` 以上

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.8.70";
+$FurTools = "Furion Tools v4.9.8.71";
 
 # 输出信息
 $copyright = @"
