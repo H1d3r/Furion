c2e1ba8968da7019dfb1012589efc6192d34fbb0
MonkSoul <monksoul@outlook.com>
2026-05-17 05:35:04 +0800

    😊 发布 `v4.9.8.72` 版本，调整底层代码规范

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.8.71";
+$FurTools = "Furion Tools v4.9.8.72";
 
 # 输出信息
 $copyright = @"
