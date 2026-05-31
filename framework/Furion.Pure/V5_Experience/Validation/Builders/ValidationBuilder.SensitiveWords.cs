0660718f8678c6a1716b86616fcc320f5f508661
MonkSoul <monksoul@outlook.com>
2026-05-31 23:31:45 +0800

    😊 改进 敏感词模块及验证器设计，完善代码注释

--- framework/Furion.Pure/V5_Experience/Validation/Builders/ValidationBuilder.SensitiveWords.cs
+++ framework/Furion.Pure/V5_Experience/Validation/Builders/ValidationBuilder.SensitiveWords.cs
@@ -31,14 +31,17 @@ public sealed partial class ValidationBuilder
     /// <summary>
     ///     添加敏感词词库
     /// </summary>
-    /// <remarks>使用默认字典名称：<see cref="SensitiveWordSanitizerFactory.DefaultName" />。</remarks>
+    /// <remarks>
+    ///     <para>使用默认字典名称：<see cref="SensitiveWordSanitizerFactory.DefaultName" />。</para>
+    ///     <para>若该名称已注册，则不会执行配置委托，也不会覆盖已有实例。</para>
+    /// </remarks>
     /// <param name="configure">自定义配置委托</param>
     /// <returns>
     ///     <see cref="ValidationBuilder" />
     /// </returns>
     public ValidationBuilder AddSensitiveWords(Action<SensitiveWordSanitizerBuilder> configure)
     {
-        SensitiveWordSanitizerFactory.GetOrCreate(configure);
+        SensitiveWordSanitizerFactory.Register(configure);
 
         return this;
     }
@@ -46,6 +49,7 @@ public sealed partial class ValidationBuilder
     /// <summary>
     ///     添加敏感词词库
     /// </summary>
+    /// <remarks>若该名称已注册，则不会执行配置委托，也不会覆盖已有实例。</remarks>
     /// <param name="dictionaryName">字典名称，不区分大小写</param>
     /// <param name="configure">自定义配置委托</param>
     /// <returns>
@@ -53,7 +57,7 @@ public sealed partial class ValidationBuilder
     /// </returns>
     public ValidationBuilder AddSensitiveWords(string dictionaryName, Action<SensitiveWordSanitizerBuilder> configure)
     {
-        SensitiveWordSanitizerFactory.GetOrCreate(dictionaryName, configure);
+        SensitiveWordSanitizerFactory.Register(dictionaryName, configure);
 
         return this;
     }
@@ -61,6 +65,7 @@ public sealed partial class ValidationBuilder
     /// <summary>
     ///     添加敏感词词库
     /// </summary>
+    /// <remarks>若该名称已注册，则不会执行配置委托，也不会覆盖已有实例。</remarks>
     /// <param name="dictionaryName">字典名称，不区分大小写</param>
     /// <param name="factory">构建 <see cref="SensitiveWordSanitizer" /> 的工厂委托</param>
     /// <returns>
@@ -68,7 +73,7 @@ public sealed partial class ValidationBuilder
     /// </returns>
     public ValidationBuilder AddSensitiveWords(string dictionaryName, Func<SensitiveWordSanitizer> factory)
     {
-        SensitiveWordSanitizerFactory.GetOrCreate(dictionaryName, factory);
+        SensitiveWordSanitizerFactory.Register(dictionaryName, factory);
 
         return this;
     }
