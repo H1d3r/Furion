fa32b2d3bc6571a12aebb81a316c3a87eb337fe1
MonkSoul <monksoul@outlook.com>
2026-06-05 14:27:45 +0800

    😊 发布 `v4.9.8.91` 版本，改进定时任务看板和 `JWT` 授权

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.8.90";
+$FurTools = "Furion Tools v4.9.8.91";
 
 # 输出信息
 $copyright = @"
