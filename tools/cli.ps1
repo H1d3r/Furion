80771f0ba9ac31b10fbd58db5e87ba266e586a45
MonkSoul <monksoul@outlook.com>
2026-06-08 20:53:42 +0800

    😊 发布 `v4.9.8.94` 版本，改进生成 `JWT` 的 `Token` 逻辑

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.8.93";
+$FurTools = "Furion Tools v4.9.8.94";
 
 # 输出信息
 $copyright = @"
