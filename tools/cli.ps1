10b155f24297cd1b912aee73fcca2d012b37b4c4
MonkSoul <monksoul@outlook.com>
2026-06-08 14:02:37 +0800

    😊 发布 `v4.9.8.93` 版本，修复流变对象处理双重 `JSON` 序列化问题

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.8.92";
+$FurTools = "Furion Tools v4.9.8.93";
 
 # 输出信息
 $copyright = @"
