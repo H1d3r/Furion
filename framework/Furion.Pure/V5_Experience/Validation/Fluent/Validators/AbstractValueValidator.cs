8e519e2661a18267c658edaef7216563f370195d
MonkSoul <monksoul@outlook.com>
2026-06-26 04:33:41 +0800

    😊 修复 数据验证 `AbstractValueValidator<>` 遗漏 `abstract` 关键字导致程序集扫描异常

--- framework/Furion.Pure/V5_Experience/Validation/Fluent/Validators/AbstractValueValidator.cs
+++ framework/Furion.Pure/V5_Experience/Validation/Fluent/Validators/AbstractValueValidator.cs
@@ -29,4 +29,4 @@ namespace Furion.Validation;
 ///     单值验证器抽象基类
 /// </summary>
 /// <typeparam name="T">对象类型</typeparam>
-public class AbstractValueValidator<T> : ValueValidator<T>;
+public abstract class AbstractValueValidator<T> : ValueValidator<T>;
